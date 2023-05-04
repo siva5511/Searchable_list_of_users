1ade6bb7307653d95d17b642618aa56cb7437f3a
siva <siva1neeli@gmail.com>
2023-05-04 09:56:27 +0000

    Users List Creation

---
+++ src/App.js
@@ -0,0 +1,41 @@
+import UserProfile from './Components/UserProfile/index'
+import './App.css'
+
+const userDetailsList = [
+  {
+    uniqueNo: 1,
+    imageUrl: 'https://assets.ccbp.in/frontend/react-js/esther-howard-img.png',
+    name: 'Sri Ram',
+    role: 'Software Developer',
+  },
+  {
+    uniqueNo: 2,
+    imageUrl: 'https://assets.ccbp.in/frontend/react-js/floyd-miles-img.png',
+    name: 'Sri Laxmi',
+    role: 'Junior Developer',
+  },
+  {
+    uniqueNo: 3,
+    imageUrl: 'https://assets.ccbp.in/frontend/react-js/jacob-jones-img.png',
+    name: 'Sri Bharathi',
+    role: 'Supporting Executive',
+  },
+  {
+    uniqueNo: 4,
+    imageUrl: 'https://assets.ccbp.in/frontend/react-js/devon-lane-img.png',
+    name: 'Sri Shatrugna',
+    role: 'System Engineer',
+  },
+]
+const App = () => (
+  <div className="list-container">
+    <h1 className="title">Users List App</h1>
+    <ul>
+      {userDetailsList.map(eachItem => (
+        <UserProfile userDetails={eachItem} key={eachItem.uniqueNo} />
+      ))}
+    </ul>
+  </div>
+)
+
+export default App
